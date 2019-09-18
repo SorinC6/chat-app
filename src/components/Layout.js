8a8f4b07523aaecef5d9fda6ad75c63c92bc3026
Sorin Chis <37213944+SorinC6@users.noreply.github.com>
2019-09-18 23:08:58 +0300

    Update Layout.js

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -3,7 +3,6 @@ import io from 'socket.io-client'
 import { USER_CONNECTED, LOGOUT } from '../Events'
 import LoginForm from './LoginForm'
 import ChatContainer from './chats/ChatContainer'
-
 const socketUrl = "http://localhost:3231"
 export default class Layout extends Component {
 	
@@ -52,8 +51,6 @@ export default class Layout extends Component {
 		this.setState({user:null})
 
 	}
-
-
 	render() {
 		const { title } = this.props
 		const { socket, user } = this.state
